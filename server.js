1a02c39f575bfd77ae33944f637fe737d82ff7bb
Devin Beliveau <devinmarieb@gmail.com>
2017-03-15 19:59:38 -0600

    I have no idea what's happening

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const reload = require('reload')
 const bodyParser = require('body-parser')
 const md5 = require('md5')
 const fs = require('fs')
+const morgan = require('morgan')
 const app = express()
 
 app.use(function(req, res, next) {
@@ -14,6 +15,7 @@ app.use(function(req, res, next) {
 })
 
 app.use(bodyParser.json())
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 // app.use('/api/folders/:id', express.static('public/urlIndex.html'))
@@ -22,8 +24,8 @@ app.set('port', process.env.PORT || 3000)
 
 app.locals.folders = [{id: '1', name: 'test'}]
 
-app.locals.urls = [{ id: '1', longURL: 'http://www.google.com', shortenedURL: 'Animals', folderID: '1'},
-{ id: '2', longURL: 'Animals.com', shortenedURL: 'Food', folderID: '1'}
+app.locals.urls = [{ id: '1', longURL: 'http://www.google.com', shortenedURL: 'Animals', folderID: 'test'},
+{ id: '2', longURL: 'Animals.com', shortenedURL: 'Food', folderID: 'test'}
 ]
 
 app.post('/api/folders', (req, res)=> {
@@ -33,21 +35,28 @@ app.post('/api/folders', (req, res)=> {
   res.json({ id, name })
 })
 
-app.post('/api/urls', (req, res)=> {
+app.post('/api/folders/:folderName/urls', (req, res)=> {
   const { longURL } = req.body
-  app.locals.urls.push({ longURL })
-  res.json({ longURL })
-})
-
-app.get('/api/folders/:id', (req, res)=> {
-  const { id } = req.params
+  const { folderID, folderName } = req.params
+  app.locals.urls.push({ longURL, folderID })
   const url = app.locals.urls.filter((url)=> {
-    return url.folderID == id
+    return url.folderID === folderName
   })
     if(!url) {
       return res.sendStatus(404)
     }
-  res.json(url)
+  res.send(url)
+})
+
+app.get('/api/folders/urls/:folderName', (req, res)=> {
+  const { folderName } = req.params
+  const url = app.locals.urls.filter((url)=> {
+    return url.folderID === folderName
+  })
+    if(!url) {
+      return res.sendStatus(404)
+    }
+  res.send(url)
 })
 
 app.get('/api/folders', (req, res)=> {
