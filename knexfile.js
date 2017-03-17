93145eb4ae452324b72ef77aa013717b42c21384
Gabi Procell <gprocell927@gmail.com>
2017-03-17 10:37:40 -0600

    adds seed files to production

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection:'postgres://localhost/jetfuel',
+    connection:postgres://jnrxchlbhgxtcu:3a2daac4dc7ae0bf85177ce1e5236c0c823e218b887a82bc61255f150a939587@ec2-50-19-83-146.compute-1.amazonaws.com:5432/d87q1a6m4vj4gp + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds/production'
     },
     useNullAsDefault: true
   },
