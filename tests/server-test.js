ec4b233eb0035f0ddb98c525f60095a2af9b74e8
Gabi Procell <gprocell927@gmail.com>
2017-03-16 20:25:18 -0600

    wip: server tests

--- tests/server-test.js
+++ tests/server-test.js
@@ -10,6 +10,7 @@ describe('Server', () => {
   it('should exist', () => {
     expect(app).to.exist
   })
+})
 
   describe('GET /', () => {
     it('should return html successfully', (done) => {
@@ -24,7 +25,41 @@ describe('Server', () => {
     })
   })
 
-  describe ('POST /api/folders', () => {
-    
+  describe ('GET /api/folders', () => {
+
+    it('should return all folders', () => {
+      chai.request(app)
+      .get('/api/folders')
+      .end((err,res) => {
+        if(err) { done(err)}
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.be.a('object')
+        done()
+        })
+      })
+    })
+
+    describe('GET /api/folders/:id/urls', () => {
+      beforeEach(done => {
+        const folders = [{id:1, name:'food'},
+        {id:2, name:'shoes'}]
+        app.locals.folders = folders
+        done()
+      })
+      context('if a folder is found', () => {
+      it.skip('should return a specific folder', (done) => {
+        chai.request(app)
+        .get('/api/folders/2')
+        .end((err, res) => {
+          if(err) {done(err)}
+          expect(res).to.have.status(200)
+          expect(res).to.be.json
+          expect(res.body).to.be.a('object')
+          expect(res.body).to.have.property('name')
+          expect(res.body.name).to.equal('shoes')
+          done()
+        })
+      })
+    })
   })
-})
