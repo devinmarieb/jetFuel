28aabc058a8af2775910067300c8c0c8ee9d6608
Gabi Procell <gprocell927@gmail.com>
2017-03-17 10:40:29 -0600

    configures environment variable for production

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection:postgres://jnrxchlbhgxtcu:3a2daac4dc7ae0bf85177ce1e5236c0c823e218b887a82bc61255f150a939587@ec2-50-19-83-146.compute-1.amazonaws.com:5432/d87q1a6m4vj4gp + `?ssl=true`,
+    connection:process.env.HEROKU_POSTGRESQL_SILVER_URL + `?ssl=true`
     migrations: {
       directory: './db/migrations'
     },
